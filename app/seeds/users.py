b7582b7fa368c68ead0fd8718f7c9b311048bb8d
mikaelkuniko <mikaelkuniko@gmail.com>
2023-10-23 11:57:55 -0700

    converting seed into db

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,13 +5,13 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', firstname='demo', lastname = 'user', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io',firstname='marnie', lastname = 'user', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', firstname='bobbie', lastname = 'user', password='password')
     mikael = User(
-        username='Mikael', email="mikael@gmail.com", password="password"
+        username='Mikael', email="mikael@gmail.com", firstname='mikael', lastname = 'kuniko',password="password"
     )
 
     db.session.add(demo)
