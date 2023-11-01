d9ba63d451da04d945a1edd48630414e54afbc52
mikaelkuniko <mikaelkuniko@gmail.com>
2023-11-01 09:40:49 -0700

    Noting process of forms and validations

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -7,13 +7,14 @@ from app.models import User
 def user_exists(form, field):
     # Checking if user exists
     email = field.data
+    # makes a vatiabel for filter use
     user = User.query.filter(User.email == email).first()
+    # loops through all users and checks if email is used
     if user:
         raise ValidationError('Email address is already in use.')
 
 
 def username_exists(form, field):
-    # Checking if username is already in use
     username = field.data
     user = User.query.filter(User.username == username).first()
     if user:
