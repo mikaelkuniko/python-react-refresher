59537cd2e63db2dbb8b8572d596c542d81ac7785
mikaelkuniko <mikaelkuniko@gmail.com>
2023-11-07 16:06:02 -0800

    explaining code

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -25,5 +25,6 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
+    # validator for having data inputted and using the function for checking a username existed
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
